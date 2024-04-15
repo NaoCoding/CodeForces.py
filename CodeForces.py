3cbca63827675b325928b22a607af8faa4c842db
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-04-15 23:05:51 +0800

    Update CodeForces.py

--- CodeForces.py
+++ CodeForces.py
@@ -1,22 +1,21 @@
 import requests
 import json
 
-def ContestsFutureNames() -> list:
-    r = []
-    a = json.loads(requests.get("https://codeforces.com/api/contest.list?gym=false").content.decode())
-    if a['status'] == 'OK':
-        for i in a['result']:
-            if i['phase'] == "BEFORE":
-                r.append(i['name'])
-    r.reverse()
-    return r
+class user:
 
-def UserGeneral(username : str):
-    a = json.loads(requests.get("https://codeforces.com/api/user.info?handles="+username).content.decode())
-    return a['result']
+    def __init__(self,username):
+        self.data = json.loads(requests.get("https://codeforces.com/api/user.info?handles="+username).content.decode())
+        self.rating = self.data['result'][0]['rating']
+        self.handle = self.data['result'][0]['handle']
+        self.country = self.data['result'][0]['country']
+        self.lastName = self.data['result'][0]['lastName']
+        self.firstName = self.data['result'][0]['firstName']
+        self.city = self.data['result'][0]['city']
+        self.friendOfCount = self.data['result'][0]['friendOfCount']
+        self.rank = self.data['result'][0]['rank']
+        self.maxRating = self.data['result'][0]['maxRating']
+        self.maxRank = self.data['result'][0]['maxRank']
+        self.organization = self.data['result'][0]['organization']
 
-def UserRating(username : str):
-    a = json.loads(requests.get("https://codeforces.com/api/user.info?handles="+username).content.decode())
-    return a['result'][0]['rating']
 
 
